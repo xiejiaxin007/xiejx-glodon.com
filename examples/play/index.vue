8875943ceede4375d29ef4d3cd96c5f92dbca5c6
GRANDSOFT\xiejx <xiejx@glodon.com>
2022-05-12 18:32:07 +0800

    refactor 添加静态数据

--- examples/play/index.vue
+++ examples/play/index.vue
@@ -12,41 +12,42 @@
     style="width: 100%"
     height="250">
       <el-table-column
-      fixed
-      prop="date"
-      label="日期"
-      width="150">
-    </el-table-column>
-    <el-table-column
-      prop="name"
-      label="姓名"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="province"
-      label="省份"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="city"
-      label="市区"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="address"
-      label="地址"
-      width="300">
-    </el-table-column>
-    <el-table-column
-      prop="zip"
-      label="邮编"
-      width="120">
-    </el-table-column>
+        fixed
+        prop="date"
+        label="日期"
+        width="150">
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="姓名"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="province"
+        label="省份"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="city"
+        label="市区"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="地址"
+        width="300">
+      </el-table-column>
+      <el-table-column
+        prop="zip"
+        label="邮编"
+        width="120">
+      </el-table-column>
     </el-table-learn>
   </div>
 </template>
 
 <script>
+  import {mockData} from './mock';
   export default {
     data() {
       return {
@@ -101,6 +102,9 @@
           zip: 200333
         }]
       };
+    },
+    mounted() {
+      this.tableData = [...this.tableData, mockData]
     }
   };
 </script>
---
+++ examples/play/mock.js
@@ -0,0 +1,16 @@
+/*
+ * @description:
+ * @author: xiejiaxin
+ * @e-mail: xiejx@glodon.com
+ * @Date: 2022-05-12 16:44:08
+ * @desc: 虚拟滚动表格数据
+ */
+/* eslint-disabled */
+export const mockData = {
+  date: '2016-05-03',
+  name: '王小虎',
+  province: '上海',
+  city: '普陀区',
+  address: '上海市普陀区金沙江路 1518 弄',
+  zip: 200333
+};
